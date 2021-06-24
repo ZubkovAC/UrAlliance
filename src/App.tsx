0985461f62d08d66b62cde08c33fbb933c92c86c
ZubkovAC <3y6kob90@gmail.com>
2021-06-24 14:35:42 +0300

    Create app (redux,react-redux,thunk)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+// react, redux, axios, visualbuilder, ant design, bootstrap
+
+function App() {
+  return (
+    <div className="App">
+        <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
